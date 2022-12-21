61811be10eba5c73e806caf9660115415565649b
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-21 22:00:01 +0530

    Latest files being uploaded at 2022-12-21-22:00:01

---
+++ Java/[473] Hours Passed [M].java
@@ -0,0 +1,47 @@
+/*
+####  Hours Passed  ####
+
+Write a function that takes time t1 and time t2 and returns the numbers of hours passed between the two times.
+
+
+[Examples]
+
+___
+hoursPassed("3:00 AM", "9:00 AM") ➞ "6 hours"
+
+hoursPassed("2:00 PM", "4:00 PM") ➞ "2 hours"
+
+hoursPassed("1:00 AM", "3:00 PM") ➞ "14 hours"
+_____
+
+
+
+[Notes]
+
+___
+*) Time t1 will always be the starting time and t2, the ending time.
+*) Return the string "No time has passed." if t1 is equal to t2.
+___
+
+
+
+[dates] [strings] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+split() Method
+https://www.geeksforgeeks.org/split-string-java-examples/
+Splits a given string around matches of the given regular expression. Learn more with different examples.
+_________
+_________
+How to Convert AM/PM to 24 Hour Time
+https://www.freecodecamp.org/news/mathematics-converting-am-pm-to-24-hour-clock/
+There are two primary methods of showing the time. First there's the 12 hour clock that uses AM and PM, and then there's the 24 hour clock. Most countries prefer the 24 …
+_________
+
+*/
+//Your code should go here:
+
---
+++ Java/[475] Percentage Changed [M].java
@@ -0,0 +1,38 @@
+/*
+####  Percentage Changed  ####
+
+Create a function that takes an old price oldPrice, a new price newPrice, and returns what percent the price decreased or increased. Round the percentage to the nearest whole percent.
+
+
+[Examples]
+
+___
+percentageChanged("$800", "$600") ➞ "25% decrease"
+
+percentageChanged("$1000", "$840") ➞ "16% decrease"
+
+percentageChanged("$100", "$950") ➞ "850% increase"
+_____
+
+
+
+[Notes]
+
+N/A
+
+
+[math] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+Calculate Percentage Increase or Decrease
+https://www.skillsyouneed.com/num/percent-change.html
+First: work out the difference (increase) between the two numbers you are comparing. Increase = New Number - Original Number Then: divide the increase by the origina …
+_________
+
+*/
+//Your code should go here:
+
