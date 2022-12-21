7b95658c348e45ca73f07a433d9b76215515bba8
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-22 00:15:01 +0530

    Latest files being uploaded at 2022-12-22-00:15:01

---
+++ C++/[388] Scalable Mountain_ [E].cpp
@@ -0,0 +1,39 @@
+/*
+####  Scalable Mountain?  ####
+
+Given an array of numbers, representing the height of a mountain in certain intervals, return whether this mountain is scalable.
+A mountain can be considered scalable if each number is within 5 units of the next number in either direction.
+
+
+[Examples]
+
+___
+isScalable([1, 2, 4, 6, 7, 8]) ➞ true
+
+isScalable([40, 45, 50, 45, 47, 52]) ➞ true
+
+isScalable([2, 9, 11, 10, 18, 21]) ➞ false
+_____
+
+
+
+[Notes]
+
+The array may start at any number and can be any length.
+
+
+[arrays] [loops] [numbers] [validation] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+std::vector
+http://www.cplusplus.com/reference/vector/vector/
+Are sequence containers representing arrays that can change in size. Just like arrays, vectors use contiguous storage locations for their elements, which means that th …
+_________
+
+*/
+//Your code should go here:
+
