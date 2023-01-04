852d16d64e737e04b77e85787a89c49875e58411
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-04 23:15:02 +0530

    Latest files being uploaded at 2023-01-04-23:15:02

---
+++ C++/[780] Find the Largest Prime within a Range [M].cpp
@@ -0,0 +1,51 @@
+/*
+####  Find the Largest Prime within a Range  ####
+
+Given two integers as arguments, create a function that finds the largest prime within the range of the two integers.
+
+
+[Examples]
+
+___
+fatPrime(2, 10) ➞ 7
+// range [2, 3, 4, 5, 6, 7, 8, 9, 10] and the largest prime is 7.
+
+fatPrime(10, 2) ➞ 7
+// [10, 9, 8, 7, 6, 5, 4, 3, 2] and the largest prime is 7.
+
+fatPrime(4, 24) ➞ 23
+// range [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] the largest prime is 23.
+_____
+
+
+
+[Notes]
+
+All numbers will be positive integers.
+
+
+[logic] [loops] [math] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+Prime Numbers
+https://www.geeksforgeeks.org/prime-numbers/
+Is a whole number greater than 1, which is only divisible by 1 and itself. First few prime numbers are : 2 3 5 7 11 13 17 19 23 …..
+_________
+_________
+C++ Program to Check Prime Number
+https://www.geeksforgeeks.org/c-program-to-check-prime-number/
+Given a positive integer, check if the number is prime or not. A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself. Example …
+_________
+_________
+max_element
+https://www.geeksforgeeks.org/max_element-in-cpp/
+We have std::max to find maximum of 2 or more elements, but what if we want to find the largest element in an array or vector or list or in a sub-section. To serve this …
+_________
+
+*/
+//Your code should go here:
+
