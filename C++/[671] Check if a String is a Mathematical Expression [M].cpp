9a8c79bc30c9092cdebd9f58bacce3b6c3ce3775
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-04 22:30:01 +0530

    Latest files being uploaded at 2023-01-04-22:30:01

---
+++ C++/[671] Check if a String is a Mathematical Expression [M].cpp
@@ -0,0 +1,42 @@
+/*
+####  Check if a String is a Mathematical Expression  ####
+
+Create a function that takes an input (e.g. "5 + 4") and returns true if it's a mathematical expression or false if not.
+
+
+[Examples]
+
+___
+mathExpr("4 + 5") ➞ true
+
+mathExpr("4*6") ➞ true
+
+mathExpr("4*no") ➞ false
+_____
+
+
+
+[Notes]
+
+___
+*) Should only work with the following operations: +, -, *, /, %
+*) You don't need to test for floats.
+*) int1 and int2 will only be from 0-9.
+___
+
+
+
+[regex] [strings] [validation] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+
+
+[No Resources]
+
+
+*/
+//Your code should go here:
+
---
+++ C++/[678] Product of All Other Numbers [M].cpp
@@ -0,0 +1,38 @@
+/*
+####  Product of All Other Numbers  ####
+
+You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+Create a function that takes an array of integers and returns an array of the products.
+
+
+[Examples]
+
+___
+getProducts([1, 7, 3, 4]) ➞ [84, 12, 28, 21]
+
+getProducts([1, 2, 6, 5, 9]) ➞ [540, 270, 90, 108, 60]
+
+getProducts([1, 2, 3, 0, 5]) ➞ [0, 0, 0, 30, 0]
+_____
+
+
+
+[Notes]
+
+You can't use division, otherwise you might end up dividing by 0 and the universe would end.
+
+
+[arrays] [loops] [math] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+
+
+[No Resources]
+
+
+*/
+//Your code should go here:
+
