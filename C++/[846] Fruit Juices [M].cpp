5d9441169e6c0571bbeb273138e7975bce75d3a3
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-06 19:01:01 +0530

    Latest files being uploaded at 2023-01-06-19:01:01

---
+++ C++/[846] Fruit Juices [M].cpp
@@ -0,0 +1,58 @@
+/*
+####  Fruit Juices  ####
+
+A fruit juice company tags their fruit juices by concatenating the first three letters of the words in a flavor's name and its capacity.
+Create a function that creates the product IDs for the variety of fruit juices.
+
+
+[Examples]
+
+___
+getDrinkID("apple", "500ml") ➞ "APP500"
+
+getDrinkID("pineapple", "45ml") ➞ "PIN45"
+
+getDrinkID("passion fruit", "750ml") ➞ "PASFRU750"
+_____
+
+
+
+[Notes]
+
+___
+*) Capacity will be given as a string and will always be given in ml.
+*) Return the product ID in UPPERCASE.
+___
+
+
+
+[loops] [strings] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+std::string::at
+http://www.cplusplus.com/reference/string/string/at/
+Returns a reference to the character at position pos in the string. The function automatically checks whether pos is the valid position of a character in the string (i …
+_________
+_________
+string::find_last_of
+http://www.cplusplus.com/reference/string/string/find_last_of/
+Searches the string for the last character that matches any of the characters specified in its arguments.
+_________
+_________
+substring
+http://www.cplusplus.com/reference/string/string/substr/
+Returns a newly constructed string object with its value initialized to a copy of a substring of this object.
+_________
+_________
+How to Split a String
+https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/#:~:text=How%20to%20split%20a%20string%20in%20C%2B%2B%201,Solution%203%20%28for%20the%20future%29%3A%20Using%20ranges.%20
+How to split a string into an array of substrings.
+_________
+
+*/
+//Your code should go here:
+
