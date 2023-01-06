d7b7e8b1bb6ad80d0f75b667daab71cc02d34bc0
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-06 18:01:01 +0530

    Latest files being uploaded at 2023-01-06-18:01:01

---
+++ C++/[835] Preventing the Collapse of the Universe [M].cpp
@@ -0,0 +1,39 @@
+/*
+####  Preventing the Collapse of the Universe  ####
+
+Dividing by 0 is a huge mistake and should be avoided at all costs.
+Create a function that when given a math expression as a string, return true if at any point, the expression involves dividing by 0.
+
+
+[Examples]
+
+___
+catchZeroDivision("2 / 0") ➞ true
+
+catchZeroDivision("4 / (2 + 3 - 5)") ➞ true
+
+catchZeroDivision("2 * 5 - 10") ➞ false
+_____
+
+
+
+[Notes]
+
+Multiplication signs will be given as an asterisk *.
+
+
+[language_fundamentals] [math] [numbers] [validation] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+std::string::at
+http://www.cplusplus.com/reference/string/string/at/
+Returns a reference to the character at position pos in the string. The function automatically checks whether pos is the valid position of a character in the string (i …
+_________
+
+*/
+//Your code should go here:
+
