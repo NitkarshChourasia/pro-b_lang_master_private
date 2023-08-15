37ad8f841de4ca54d8115674ed7de70ed3fb0e37
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-08-15 19:51:07 +0530

    Adding C++ very_easy 57 programs problems.

---
+++ C++/very_easy/[1] Return the Sum of Two Numbers [VE].cpp
@@ -0,0 +1,63 @@
+/*
+####  Return the Sum of Two Numbers  ####
+
+Create a function that takes two numbers as arguments and returns their sum.
+
+
+[Examples]
+
+___
+addition(3, 2) ➞ 5
+
+addition(-3, -6) ➞ -9
+
+addition(7, 3) ➞ 10
+_____
+
+
+
+[Notes]
+
+___
+*) Don't forget to return the result.
+*) If you get stuck on a challenge, find help in the Resources tab.
+*) If you're really stuck, unlock solutions in the Solutions tab.
+___
+
+
+
+[algebra] [language_fundamentals] [math] [numbers] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+Understand C++ Functions With Examples
+https://www.programiz.com/cpp-programming/function
+In this article, you'll learn everything about functions in C++; what type of functions are there, how to use them with examples.
+_________
+_________
+Functions
+http://www.cplusplus.com/doc/tutorial/functions/
+Functions allow to structure programs in segments of code to perform individual tasks.
+_________
+_________
+Pluralsight C++
+https://www.pluralsight.com/paths/c-plus-plus
+If you’re just learning to program you can get a gentle introduction with Learn How to Program with C++ . If you already know how to program and just want to learn C++, …
+_________
+_________
+Operators in C / C++
+https://www.geeksforgeeks.org/operators-c-c/
+Operators are the foundation of any programming language. Thus the functionality of C/C++ programming language is incomplete without the use of operators. We can define …
+_________
+_________
+How to Add
+https://www.wikihow.com/Add
+This sounds like a weird link, but it might actually help you solve this challenge if you're stuck!
+_________
+
+*/
+//Your code should go here:
+
