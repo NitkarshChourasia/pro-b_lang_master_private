86a0330641b6a8cb371a5cabfe738a1923e4fdfc
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-08-15 19:50:00 +0530

    Changing the parameters of the scraping code, to continue scraping of C++ programs problems.

--- Miscellaneous/scraping_code/standarised_scraping_code_new_latest_mod.js
+++ Miscellaneous/scraping_code/standarised_scraping_code_new_latest_mod.js
@@ -1,7 +1,7 @@
 // Finalised Script Done.
 //Final script
-var starting_program_index = 1;
-var number_of_program_done_in_Cur_dif = 0;
+var starting_program_index = 57;
+var number_of_program_done_in_Cur_dif = 56;
 var current_difficulty = "Very Easy"; // "Very Easy" "Easy" "Medium" "Hard" "Very Hard" "Expert"
 var prog_lang = "c++"; // py js java c++ c#
 
