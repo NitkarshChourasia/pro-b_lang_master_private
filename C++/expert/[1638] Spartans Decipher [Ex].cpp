041029948d66b60615a41955b7a7a3af0d1fdc22
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-08-21 14:28:55 +0530

    C++ 4th Checkpoint, Adding C++ programs after downloading to expert difficulty.

---
+++ C++/expert/[1638] Spartans Decipher [Ex].cpp
@@ -0,0 +1,47 @@
+/*
+####  Spartans Decipher  ####
+
+In Spartans Cipher, encoding is done by writing the text horizontally, across the strap in the plaintext word of a message. In ancient times, Spartans and Greeks invented interesting way of encryption called Scytale. The detailed description of the encoding algorithm is provided by Mubashir in his challenge. You are encouraged to solve that one first.
+In this challenge, the function is given the encoded message and the number of slides. Create a reversed solution to retrieve the original message.
+
+
+[Examples]
+
+___
+spartansDecipher("Ms t euhSaC biclo aryed", 6) ➞ "Mubashir Scytale Code"
+
+spartansDecipher("M  baai aaEirmn tndteag tda  z", 8) ➞ "Matt and Edabit are amazing"
+_____
+
+
+
+[Notes]
+
+Strip white spaces on the right of the resulting string, because they were added during the encoding.
+
+
+[algorithms] [cryptography] [logic] [strings] 
+
+
+
+-------------------------------------------------------------------
+[Resources]
+_________
+Scytale
+https://en.wikipedia.org/wiki/Scytale
+A tool used to perform a transposition cipher, consisting of a cylinder with a strip of parchment wound around it on which is written a message. The ancient Greeks, and …
+_________
+_________
+Ceil and Floor Functions
+https://www.geeksforgeeks.org/ceil-floor-functions-cpp/
+In mathematics and computer science, the floor and ceiling functions map a real number to the greatest preceding or the least succeeding integer, respectively.
+_________
+_________
+std::string::substr
+http://www.cplusplus.com/reference/string/string/substr/
+Returns a newly constructed string object with its value initialized to a copy of a substring of this object. The substring is the portion of the object that starts at …
+_________
+
+*/
+//Your code should go here:
+
